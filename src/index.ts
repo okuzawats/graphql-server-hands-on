169db2206d3065bdd1234aeb8edfb689bbd9b9bc
okuzawats <okuzawats@gmail.com>
2025-01-25 16:42:40 +0900

    フォーマットの修正

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,25 @@ const schema = loadSchemaSync(
 
 const resolvers = {
     Query: {
-        authors: () => Object.entries(db.users).map(([id, user]) => ({ id, ...user })),
+        authors: () => Object
+            .entries(db.users)
+            .map(([id, user]) => (
+                { id, ...user }
+            )),
     },
     Post: {
-        tags: ({ tags }) => tags.map((name) => ({ name })),
+        tags: ({ tags }) => tags
+            .map((name) => (
+                { name }
+            )),
     },
     Author: {
-        posts: ({ id }) => Object.entries(db.blogs).filter(([_, post]) => post.author === id).map(([id, post]) => ({ id, ...post })),
+        posts: ({ id }) => Object
+            .entries(db.blogs)
+            .filter(([_, post]) => post.author === id)
+            .map(([id, post]) => (
+                { id, ...post }
+            )),
     },
 };
 
