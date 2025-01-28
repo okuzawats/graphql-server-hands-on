b085fae521f10efc58c99e6784b2900ed17d62b7
okuzawats <okuzawats@gmail.com>
2025-01-28 22:22:36 +0900

    dataloaderの実装をデフォルトに差し替え

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,72 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { addResolversToSchema } from '@graphql-tools/schema';
-import { db } from './database.js';
+import { authors, posts, tags } from './database.js';
+import DataLoader from 'dataloader';
 
-const schema = loadSchemaSync(
-    'schema.graphql',
-    {
-        loaders: [new GraphQLFileLoader()]
+const schema = loadSchemaSync('schema.graphql', { loaders: [new GraphQLFileLoader()] });
+
+const getAuthors = () => {
+    // console.log(`SELECT * FROM author`);
+    return authors
+};
+
+class PostsDataSource {
+    private loader = new DataLoader(async (keys: number[]) => {
+        // console.log(`SELECT * FROM post WHERE author_id IN (${keys.join(",")})`);
+        const results = posts.filter(post => keys.includes(post.author_id));
+        return keys.map((key: number) => results.filter(post => post.author_id === key));
+    });
+
+    async getPostsBy(author_id: number) {
+        return this.loader.load(author_id);
     }
-);
+}
+
+class TagsDataSource {
+    private loader = new DataLoader(async (keys: number[]) => {
+        // console.log(`SELECT * FROM tag WHERE post_id IN (${keys.join(",")})`);
+        const results = tags.filter(tag => keys.includes(tag.post_id));
+        return keys.map((key: number) => results.filter(tag => tag.post_id === key));
+    });
+
+    async getTagsBy(post_id: number) {
+        return this.loader.load(post_id);
+    }
+}
+
+interface ContextValue {
+    dataSources: {
+        posts: PostsDataSource;
+        tags: TagsDataSource;
+    };
+}
 
 const resolvers = {
     Query: {
-        authors: () => Object
-            .entries(db.users)
-            .map(([id, user]) => (
-                { id, ...user }
-            )),
-    },
-    Post: {
-        tags: ({ tags }) => tags
-            .map((name) => (
-                { name }
-            )),
+        authors: () => getAuthors(),
     },
     Author: {
-        posts: ({ id }) => Object
-            .entries(db.blogs)
-            .filter(([_, post]) => post.author === id)
-            .map(([id, post]) => (
-                { id, ...post }
-            )),
+        posts: (parent, _, { dataSources }) => dataSources.posts.getPostsBy(parent.id),
+    },
+    Post: {
+        tags: (parent, _, { dataSources }) => dataSources.tags.getTagsBy(parent.id),
     },
 };
 
 const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
-const apolloServer = new ApolloServer({ schema: schemaWithResolvers });
-const { url } = await startStandaloneServer(apolloServer);
+
+const apolloServer = new ApolloServer<ContextValue>({ schema: schemaWithResolvers });
+
+const { url } = await startStandaloneServer(apolloServer, {
+    context: async () => {
+        return {
+            dataSources: {
+                posts: new PostsDataSource(),
+                tags: new TagsDataSource(),
+            },
+        };
+    },
+});
 
 console.log(`🚀 Apollo Server started: ${url}`);
